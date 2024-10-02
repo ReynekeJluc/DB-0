78bc0907806e1b41d6769f69edd9dd3aa7a7cb6c
Reinecke_Fox <72546226+ReynekeJluc@users.noreply.github.com>
2024-10-02 10:36:34 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize("postgresql://postgres.rzylrywyevfvduerkzdp:Andrenov2003@aws-0-eu-central-1.pooler.supabase.com:6543/postgres", { dialect: 'postgres' });
+const sequelize = new Sequelize("", { dialect: 'postgres' });
 
 export default sequelize;
