1712472a90bea197843c880da7858b2762eb1900
Andrenov Ruslan <ruslan.andrenov2003@gmail.com>
2024-10-02 20:57:35 +0800

    fix

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize';
 const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
-	`postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}.oregon-postgres.render.com/${DB_NAME}?ssl=true`,
+	`postgresql://db_vnss_user:odvSKqEe0uNAN5zppSPUMwASfpbeauy5@dpg-cruiooogph6c73aj72hg-a.oregon-postgres.render.com/db_vnss?ssl=true`,
 	{ dialect: 'postgres' }
 );
 
