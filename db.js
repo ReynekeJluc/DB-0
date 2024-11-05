b36dfdf24713b92ba1c0dd3c76ff425387e281c3
Andrenov Ruslan <ruslan.andrenov2003@gmail.com>
2024-11-05 17:48:21 +0800

    fix connection (new db)

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize';
 //const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
-	`postgresql://db_vnss_user:odvSKqEe0uNAN5zppSPUMwASfpbeauy5@dpg-cruiooogph6c73aj72hg-a.oregon-postgres.render.com/db_vnss?ssl=true`,
+	`postgresql://db_a10c_user:A0khoNqbLhlUvzuv7hlR3aZAWp0au3s3@dpg-csksr2u8ii6s7380n160-a.oregon-postgres.render.com/db_a10c?ssl=true`,
 	{ dialect: 'postgres' }
 );
 
