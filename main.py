0fddd1277ab2ecd990c691d4134a12f119c9d044
Andrenov Ruslan <ruslan.andrenov2003@gmail.com>
2024-12-05 20:45:44 +0800

    the initial version of the procedures

---
+++ main.py
@@ -0,0 +1,145 @@
+import pg8000
+
+class ProceduresCrud:
+		def __init__(self, db_conn):
+				self.conn = db_conn
+				self.cursor = self.conn.cursor()
+      
+					
+		def add_provider(self, name, address, phone, email):
+				try:
+						self.cursor.execute("CALL add_provider(%s, %s, %s, %s)", (name, address, phone, email))
+						self.conn.commit()
+						print("Провайдер успешно добавлен")
+				except Exception as e:
+						self.conn.rollback()
+						print(e)
+						
+
+		def get_all_providers(self):
+				self.cursor.execute("SELECT * FROM get_all_providers()")
+				rows = self.cursor.fetchall()
+				max_length = [0] * len(rows[0])
+				
+				for row in rows:
+						for i, value in enumerate(row):
+								max_length[i] = max(max_length[i], len(str(value)))
+				format_string = " ".join(["{:<" + str(length + 3) + "}" for length in max_length])
+
+				for row in rows:
+						print(format_string.format(*row))
+						
+
+		def get_provider_by_id(self, id):
+				try:
+						self.cursor.execute("SELECT * FROM get_provider_by_id(%s)", (id,))
+						result = self.cursor.fetchone()
+						if result:
+								print("{:<20} {}".format("id:", result[0]))
+								print("{:<20} {}".format("name:", result[1]))
+								print("{:<20} {}".format("address:", result[2]))
+								print("{:<20} {}".format("phone:", result[3]))
+								print("{:<20} {}".format("email:", result[4]))
+						else:
+								print(f"Провайдер с id = {id} не найден")
+				except Exception as e:
+						self.conn.rollback()
+						print(e)
+						
+						
+		def update_provider(self, id, name, address, phone, email):
+				try:
+						self.cursor.execute("CALL update_provider(%s, %s, %s, %s, %s)",
+								(id.strip(), name, address, phone, email))
+						self.conn.commit()
+
+						print("Провайдер успешно обновлен")
+				except Exception as e:
+						self.conn.rollback()
+						print(e)
+						
+
+		def delete_provider(self, id):
+				try:
+						self.cursor.execute("CALL delete_provider(%s)", (id.strip(),))
+						self.conn.commit()
+
+						print("Провайдер успешно удален")
+				except Exception as e:
+						self.conn.rollback()
+						print(e)
+						
+
+		def delete_list_providers(self, ids):
+				try:
+						self.cursor.execute("SELECT delete_list_providers(%s)", (ids,))
+						deleted_count = self.cursor.fetchone()[0]
+						self.conn.commit()
+
+						print(f"Удалено провайдеров: {deleted_count}")
+				except Exception as e:
+						self.conn.rollback()
+						print(e)
+        
+
+
+def show_menu():
+		print("\033[31m\nМеню:\033[0m")
+		print("\033[32m1\033[0m. Добавить провайдера")
+		print("\033[32m2\033[0m. Вывести всех провайдеров")
+		print("\033[32m3\033[0m. Вывести провайдера по id")
+		print("\033[32m4\033[0m. Изменить провайдера по id")
+		print("\033[32m5\033[0m. Удалить провайдера по id")
+		print("\033[32m6\033[0m. Удалить список провайдеров")
+		print("\033[32m0\033[0m. Выйти")
+        
+
+
+def main():
+		conn = pg8000.connect(database="db_a10c", user="db_a10c_user", password="A0khoNqbLhlUvzuv7hlR3aZAWp0au3s3", host="dpg-csksr2u8ii6s7380n160-a.oregon-postgres.render.com", port="5432")
+					
+		crud = ProceduresCrud(conn)
+
+		while True:
+				show_menu()
+				choice = input("\033[33mВыберите операцию: \033[0m")
+				
+				match choice.strip():
+						case "1":
+								name = input("Введите название провайдера: ")
+								address = input("Введите адрес провайдера: ")
+								phone = input("Введите номер провайдера: ")
+								email = input("Введите почту провайдера: ")
+								crud.add_provider(name, address, phone, email)
+						case "2":
+								crud.get_all_providers()
+						case "3":
+								id = input("Введите id провайдера: ")
+								crud.get_provider_by_id(id)
+						case "4":
+								id = input("Введите id провайдера для обновления: ")
+								name = input("Введите новое название провайдера: ")
+								address = input("Введите новый адрес: ")
+								phone = input("Введите новый номер: ")
+								email = input("Введите новый email: ")
+								crud.update_provider(id, name, address, phone, email)
+						case "5":
+								id = input("Введите id провайдера для удаления: ")
+								crud.delete_provider(id)
+						case "6":
+								ids_input = input("Введите id провайдеров для удаления (через пробел): ")
+								ids = ids_input.split()
+								crud.delete_list_providers(ids)
+						case "0":
+								print("\033[31mВыход из программы\033[0m")
+								break
+						case _:
+								print("\033[31mЧто-то пошло не так\033[0m")
+						
+		conn.close()
+
+
+
+
+if __name__ == "__main__":
+    main()
--- migrations/versions/2d155b06ede1_procedures.py
+++ migrations/versions/2d155b06ede1_procedures.py
@@ -20,8 +20,171 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    pass
+    #! Исправить
+    op.execute(
+    """
+        CREATE OR REPLACE PROCEDURE add_provider(
+            IN name TEXT,
+            IN address TEXT,
+            IN phone TEXT
+            IN email TEXT
+        )
+        LANGUAGE plpgsql AS $$
+            BEGIN
+                name := TRIM(name);
+                address := TRIM(address);
+                            
+                INSERT INTO manufacturer (title_country, address_of_manufacturer, contact_list) 
+                VALUES (name, address, phone, email);
+            END;
+        $$;
+    """)
+	
+    op.execute(
+        """
+            CREATE OR REPLACE PROCEDURE get_all_providers()
+            LANGUAGE plpgsql AS $$
+                BEGIN
+                    RETURN QUERY SELECT * FROM providers;
+                END;
+            $$;
+        """
+    )
+
+    #! Исправить
+    op.execute(
+        """
+            CREATE OR REPLACE FUNCTION get_provider_by_id(IN id TEXT)
+            RETURNS TABLE(id INT, name VARCHAR(255), address TEXT, phone TEXT, email TEXT) AS $$
+                DECLARE
+                    id_new INT;
+                BEGIN
+                    id := TRIM(id);
+
+                    IF LENGTH(id) = 0 OR NOT id ~ '^[0-9]+$' THEN
+                        RAISE EXCEPTION 'The identifier must be a positive integer and cannot be empty';
+                    END IF;
+                    
+                    id_new := CAST(id AS INT);
+                    
+                    IF NOT EXISTS (SELECT 1 FROM providers WHERE providers.id = id_new) THEN
+                        RAISE EXCEPTION 'Provider with id % not found', id_new;
+                    END IF;
+                    
+                    RETURN QUERY SELECT * FROM providers WHERE providers.id = id_new;
+                END;
+            $$ LANGUAGE plpgsql;
+        """
+    )
+
+    #! Исправить
+    op.execute(
+    """
+        CREATE OR REPLACE PROCEDURE update_provider(
+            IN id TEXT, 
+            IN name TEXT,
+            IN address TEXT,
+            IN phone TEXT
+            IN email TEXT
+        )
+        LANGUAGE plpgsql AS $$
+            BEGIN
+                IF LENGTH(TRIM(p_manufacturer_id)) = 0 OR NOT TRIM(p_manufacturer_id) ~ '^[0-9]+$' THEN
+                    RAISE EXCEPTION 'The provider id must be a positive integer and cannot be empty';
+                END IF;
+                DECLARE
+                    id_new INT := CAST(TRIM(id) AS INT);
+                BEGIN
+                    IF NOT EXISTS (SELECT 1 FROM providers WHERE id = id_new) THEN
+                        RAISE EXCEPTION 'Provider with id % not found', id_new;
+                    END IF;
+
+                    IF name IS NOT NULL AND TRIM(name) <> '' THEN
+                        UPDATE providers 
+                        SET name = TRIM(name)
+                        WHERE id = id_new;
+                    END IF;
+
+                    IF address IS NOT NULL AND TRIM(address) <> '' THEN
+                        UPDATE providers 
+                        SET address = TRIM(address)
+                        WHERE id = id_new;
+                    END IF;
+                    
+                    IF phone IS NOT NULL AND TRIM(phone) <> '' THEN
+                        UPDATE providers 
+                        SET phone = TRIM(phone)
+                        WHERE id = id_new;
+                    END IF;
+                END;
+            END;
+        $$;
+    """)
+
+    #! Исправить
+    op.execute(
+        """
+            CREATE OR REPLACE PROCEDURE delete_provider(IN id TEXT)
+            LANGUAGE plpgsql AS $$
+            DECLARE
+                id_new INT;
+            BEGIN
+                id_new := NULLIF(TRIM(id), '');
+                IF id_new IS NULL OR NOT (id ~ '^[0-9]+$') THEN
+                    RAISE EXCEPTION 'The provider id must be a positive integer and cannot be empty';
+                END IF;
+
+                id_new := CAST(id_new AS INT);
+
+                IF NOT EXISTS (SELECT 1 FROM providers WHERE providers.providers_id = id_new) THEN
+                    RAISE EXCEPTION 'Providers with ID % not found', id_new;
+                END IF;
+
+                DELETE FROM providers WHERE providers.providers_id = id_new;
+            END;
+            $$;
+        """
+    )
+
+    #! Исправить
+    op.execute(
+        """
+            CREATE OR REPLACE FUNCTION delete_list_providers(ids TEXT[])
+            RETURNS INTEGER AS $$
+            DECLARE
+                id_new INT;
+                cnt INT := 0; 
+                id TEXT; 
+            BEGIN
+                FOREACH id IN ARRAY ids LOOP
+                    IF id IS NULL OR NOT id ~ '^[0-9]+$' THEN
+                        RAISE NOTICE 'The provider id must be a positive integer and cannot be empty', id;
+                        CONTINUE; 
+                    END IF;
+
+                    id_new := id::INT;
+
+                    IF EXISTS (SELECT 1 FROM providers WHERE id = id_new) THEN
+                        DELETE FROM providers WHERE id = id_new;
+                        cnt := cnt + 1;  
+                    ELSE
+                        RAISE NOTICE 'The providers with id % was not found.', id_new;
+                    END IF;
+
+                END LOOP;
+                RAISE NOTICE 'Removed providers: %', cnt;
+
+                RETURN cnt;  
+            END;
+            $$ LANGUAGE plpgsql;
+        """
+    )
 
 
 def downgrade() -> None:
-    pass
+    op.execute("DROP PROCEDURE IF EXISTS add_provider(TEXT, TEXT, TEXT, TEXT);")
+    op.execute("DROP PROCEDURE IF EXISTS get_all_providers();")
+    op.execute("DROP PROCEDURE IF EXISTS get_provider_by_id(TEXT);")
+    op.execute("DROP PROCEDURE IF EXISTS update_provider(TEXT, TEXT, TEXT, TEXT, TEXT);")
+    op.execute("DROP PROCEDURE IF EXISTS delete_provider(TEXT);")
+    op.execute("DROP PROCEDURE IF EXISTS delete_list_providers(TEXT[]);")
